10f8c88d542821ce513f51db3cc0da293192f399
Ontos-2021 <josemiguel.mercado17@gmail.com>
2021-03-05 12:30:45 -0300

    Agrego la base de datos y un archivo de test

--- Recetapp.py
+++ Recetapp.py
@@ -1,8 +1,23 @@
-import sqlite3
 
-conn = sqlite3.connect("Recetapp.db")
+import mariadb
+# Module Imports
+import mariadb
+import sys
 
-cursor = conn.cursor()
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="db_user",
+        password="db_user_passwd",
+        host="192.0.2.1",
+        port=3306,
+        database="employees"
 
-conn.close()
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
 
+# Get Cursor
+print("Hola")
+cur = conn.cursor()
