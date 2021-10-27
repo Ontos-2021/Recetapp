9a6d658021a2f380a8f5d4c08d98d60e135e152b
_Ontos_ <josemiguel.mercado17@gmail.com>
2021-10-27 04:21:42 -0300

    Registre las tablas en el sitio de administrador

--- Recetapp/Apps/Recetas/admin.py
+++ Recetapp/Apps/Recetas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Recetapp.Apps.Recetas.models import *
 
 # Register your models here.
+
+admin.site.register(Receta)
+admin.site.register(Tienda)
+admin.site.register(Unidad)
+admin.site.register(Ingrediente)
