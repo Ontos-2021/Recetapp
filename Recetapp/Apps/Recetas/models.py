7d81cea2e5b9396f177a297e03c717663f866c14
_Ontos_ <josemiguel.mercado17@gmail.com>
2021-10-27 17:36:31 -0300

    Sobreescribi la funcion str del modelo

--- Recetapp/Apps/Recetas/models.py
+++ Recetapp/Apps/Recetas/models.py
@@ -7,16 +7,25 @@ class Unidad(models.Model):
     nombre = models.CharField(max_length=45)
     abreviacion = models.CharField(max_length=7)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Tienda(models.Model):
     nombre = models.CharField(max_length=45)
     horario = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.nombre
 
 
 class Ingrediente(models.Model):
     nombre = models.CharField(max_length=50)
     unidad = models.ForeignKey(Unidad, on_delete=models.DO_NOTHING)
     tienda = models.ManyToManyField(Tienda)
+
+    def __str__(self):
+        return self.nombre
 
 
 class Receta(models.Model):
@@ -25,3 +34,6 @@ class Receta(models.Model):
     vegano = models.BooleanField
     ingrediente = models.ManyToManyField(Ingrediente)
     cantidad = models.IntegerField
+
+    def __str__(self):
+        return self.nombre
