b0607a86ed339810492e5b0af3b3cfc7c9b8f054
_Ontos_ <josemiguel.mercado17@gmail.com>
2021-10-29 02:18:56 -0300

    Modifique el modelo de la base de datos acorde al Modelo hecho en el diagrama.

--- Recetapp/Apps/Recetas/models.py
+++ Recetapp/Apps/Recetas/models.py
@@ -22,7 +22,6 @@ class Tienda(models.Model):
 class Ingrediente(models.Model):
     nombre = models.CharField(max_length=50)
     unidad = models.ForeignKey(Unidad, on_delete=models.DO_NOTHING)
-    tienda = models.ManyToManyField(Tienda)
 
     def __str__(self):
         return self.nombre
@@ -32,8 +31,21 @@ class Receta(models.Model):
     nombre = models.CharField(max_length=50)
     tiempo = models.IntegerField(blank=True, null=True)
     vegano = models.BooleanField(null=True)
-    ingrediente = models.ManyToManyField(Ingrediente)
-    cantidad = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.nombre
+
+
+class RecetIngrediente(models.Model):
+    receta = models.ForeignKey(Receta, on_delete=models.CASCADE)
+    ingrediente = models.ForeignKey(Ingrediente, on_delete=models.CASCADE)
+    cantidad = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return "Receta: " + str(self.receta) + " | " + str(self.cantidad) + " " + str(self.ingrediente)
+
+
+class TiendaIngredientes(models.Model):
+    tienda = models.ForeignKey(Tienda, on_delete=models.CASCADE)
+    ingrediente = models.ForeignKey(Ingrediente, on_delete=models.CASCADE)
+    precio = models.FloatField()
