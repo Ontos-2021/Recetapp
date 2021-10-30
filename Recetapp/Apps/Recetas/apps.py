0cf35efe261dc8c5e52a1ee4a913cc3373c6d262
_Ontos_ <jose.mercado@davinci.edu.ar>
2021-10-30 04:43:04 -0300

    Reparé el BUG que no me permitía importar el módulo en Windows. Había que configurar el nombre de la ruta en Apps.py

--- Recetapp/Apps/Recetas/apps.py
+++ Recetapp/Apps/Recetas/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RecetasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Recetas'
+    name = 'Recetapp.Apps.Recetas'
